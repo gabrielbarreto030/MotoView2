94ed69e0538f65a05a84912d82c5e1b9de22b8da
barrosogamer <gabrielbarreto030@gmail.com>
2020-02-07 13:24:48 -0300

    Implementado Campo de Seleção de Horario

--- lib/screen/Agendamento.dart
+++ lib/screen/Agendamento.dart
@@ -9,9 +9,52 @@ class Agendamento extends StatefulWidget{
   }
 }
 
+class Horario{
+  int id;
+  String hora;
+
+  Horario(this.id,this.hora);
+
+  static List<Horario> getHorario (){
+    return <Horario>[
+      Horario(1,"07:00"),
+      Horario(1,"08:00"),
+      Horario(1,"09:00"),
+      Horario(1,"10:00"),
+      Horario(1,"11:00"),
+      Horario(1,"12:00"),
+      Horario(1,"13:00"),
+      Horario(1,"14:00"),
+      Horario(1,"15:00"),
+      Horario(1,"16:00"),
+      Horario(1,"17:00"),
+      Horario(1,"18:00"),
+      Horario(1,"19:00"),
+      ];
+  }
+}
 
 class StateAgendamento extends State<Agendamento>{
-   DateTime _date=DateTime.now();  
+   DateTime _date=DateTime.now();//Pegar Data Atual
+   List<Horario> _horarios=Horario.getHorario();
+   List<DropdownMenuItem<Horario>> _dropdownhorarios;
+   Horario _selectedHorario;
+   
+   @override
+   void initState(){
+     _dropdownhorarios=Builddrophorarios(_horarios);
+     _selectedHorario=_dropdownhorarios[0].value;
+     super.initState();
+   }
+
+   List<DropdownMenuItem<Horario>> Builddrophorarios(List horarios){
+       List<DropdownMenuItem<Horario>> horas = List();
+
+       for(Horario hora in horarios){
+         horas.add(DropdownMenuItem(value: hora,child: Text(hora.hora)));
+       }
+       return horas;
+   }
 
    Future<Null> FazData(BuildContext context) async {
       final DateTime DataClick= await showDatePicker(
@@ -26,6 +69,11 @@ class StateAgendamento extends State<Agendamento>{
           print(_date.toString());
         });
       }
+    }
+    ChangedHorario(Horario selectHoraio){
+        setState(() {
+          _selectedHorario=selectHoraio;
+        });
     }
   @override
   Widget build(BuildContext context) {
@@ -47,6 +95,18 @@ class StateAgendamento extends State<Agendamento>{
             style: TextStyle(fontSize: 20)
           ),
         ),
+        Container(
+          child: Column(children: <Widget>[
+            Text("Horario"),
+            DropdownButton(
+              value: _selectedHorario,
+              items: _dropdownhorarios, onChanged: ChangedHorario),
+            SizedBox(height: 40.0),
+            Text("Dia:DataSelecionada Hora:${_selectedHorario.hora}")
+
+
+          ],),
+        )
         ]
       ),
 
